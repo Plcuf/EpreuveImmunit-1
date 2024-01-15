683781aaac04e731159b69c397c518b4620f4931
Plcuf <romeo.sanchez@ynov.com>
2024-01-15 09:30:11 +0100

    fini

---
+++ controller/foncs.go
@@ -0,0 +1,17 @@
+package controller
+
+import "fmt"
+
+func dechiffrage(code string) string {
+	decryptage := ""
+	for _, i := range code {
+		letter := int(i)
+		letter += 11
+		letter += 13
+		letter -= 712 % 77
+		letter -= 3
+		decryptage += string(letter)
+	}
+	fmt.Println(decryptage)
+	return decryptage
+}
