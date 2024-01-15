1efe9fd081849a21165974e840975a5d51302639
Plcuf <romeo.sanchez@ynov.com>
2024-01-15 10:04:40 +0100

    sjdh

--- controller/foncs.go
+++ controller/foncs.go
@@ -6,8 +6,8 @@ func dechiffrage(code string) string {
 	decryptage := ""
 	for _, i := range code {
 		letter := int(i)
-		letter += 12
-		letter += 13
+		letter += 11
+		letter += 9
 		letter -= 712 % 77
 		letter -= 3
 		decryptage += string(letter)
